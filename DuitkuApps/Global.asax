d71d08a0952910dac1e19beb5a3797ee2f6d37e0
ernandarn <ernandarn@gmail.com>
2017-04-09 20:16:03 +0700

    Add project files.

---
+++ DuitkuApps/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DuitkuApps.MvcApplication" Language="C#" %>
---
+++ DuitkuApps/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DuitkuApps.Startup))]
+namespace DuitkuApps
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
